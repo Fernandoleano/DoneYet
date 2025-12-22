aefc79ab9a73f0e453bab89ab2dffb1fab61880c
FernandoLeano <fernandoleano4@gmail.com>
2025-12-22 15:21:36 -0500

    Add time-based personalized greeting to dashboard
    - Added greeting_for_time_of_day helper method
    - Dashboard now shows 'Good morning/afternoon/evening [FirstName]'
    - Added 'Here is your debriefing' subtitle for spy theme
    - Greeting changes based on time: morning (5-11), afternoon (12-17), evening (18+)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def greeting_for_time_of_day
+    hour = Time.current.hour
+
+    case hour
+    when 5..11
+      "Good morning"
+    when 12..17
+      "Good afternoon"
+    when 18..21
+      "Good evening"
+    else
+      "Good evening" # Late night/early morning
+    end
+  end
 end
