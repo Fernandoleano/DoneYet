1d5da06a1c34a10a1488bd638dd8cccbb3caf7f7
FernandoLeano <fernandoleano4@gmail.com>
2025-12-22 16:21:48 -0500

    Complete team chat system with UI
    Features:
    - Full Slack-like chat interface
    - 2-column layout (channels + messages)
    - Auto-creates #general and #random on first visit
    - Auto-joins all workspace users to default channels
    - Create custom channels
    - Send messages with file attachments
    - Real-time message display
    - User avatars and timestamps
    - Auto-scroll to latest message
    
    Controllers:
    - ChannelsController with index, show, create
    - ChatMessagesController with create
    - Auto-create default channels logic
    
    UI:
    - Channel sidebar with list
    - Message feed with user info
    - Message composer with file upload
    - New channel modal
    - Empty states
    - Responsive design
    
    Navigation:
    - Added Chat link to sidebar
    - Chat bubble icon
    
    Routes:
    - /channels - channel list (redirects to first channel)
    - /channels/:id - view channel and messages
    - /channels/:id/messages - post message
    
    This is now a fully functional team chat system!

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,16 +1,61 @@
 class ChannelsController < ApplicationController
   def index
+    @channels = Current.user.workspace.channels.order(created_at: :asc)
+
+    # Create default channels if none exist
+    if @channels.empty?
+      general = Current.user.workspace.channels.create!(
+        name: "general",
+        description: "General team discussion",
+        private: false,
+        created_by: Current.user
+      )
+
+      random = Current.user.workspace.channels.create!(
+        name: "random",
+        description: "Random chatter",
+        private: false,
+        created_by: Current.user
+      )
+
+      # Auto-join all workspace users to default channels
+      Current.user.workspace.users.each do |user|
+        general.add_member(user)
+        random.add_member(user)
+      end
+
+      @channels = Current.user.workspace.channels.order(created_at: :asc)
+    end
+
+    # Redirect to first channel
+    redirect_to channel_path(@channels.first) if @channels.any?
   end
 
   def show
+    @channel = Current.user.workspace.channels.find(params[:id])
+    @channels = Current.user.workspace.channels.order(created_at: :asc)
+    @messages = @channel.chat_messages.includes(:user).recent
+    @new_message = ChatMessage.new
+
+    # Auto-join user if not a member
+    @channel.add_member(Current.user) unless @channel.members.include?(Current.user)
   end
 
   def create
+    @channel = Current.user.workspace.channels.build(channel_params)
+    @channel.created_by = Current.user
+
+    if @channel.save
+      @channel.add_member(Current.user)
+      redirect_to channel_path(@channel), notice: "Channel created!"
+    else
+      redirect_to channels_path, alert: "Failed to create channel: #{@channel.errors.full_messages.join(', ')}"
+    end
   end
 
-  def join
-  end
+  private
 
-  def leave
+  def channel_params
+    params.require(:channel).permit(:name, :description, :private)
   end
 end
--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -1,4 +1,19 @@
 class ChatMessagesController < ApplicationController
   def create
+    @channel = Current.user.workspace.channels.find(params[:channel_id])
+    @message = @channel.chat_messages.build(message_params)
+    @message.user = Current.user
+
+    if @message.save
+      redirect_to channel_path(@channel)
+    else
+      redirect_to channel_path(@channel), alert: "Failed to send message"
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:chat_message).permit(:content, attachments: [])
   end
 end
